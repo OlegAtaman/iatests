18f8c33a51ae2f2f2c22fdf98248f1efdd338a79
OlegAtaman <aoleg1706@gmail.com>
2022-10-23 14:53:45 +0300

    added registraton

---
+++ users/forms.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
+from django import forms
+from django.utils.translation import gettext_lazy as _
+
+User = get_user_model()
+
+
+class UserCreationForm(UserCreationForm):
+
+    CHOICES = [('S', 'Студент'), ('T', 'Викладач')]
+
+    status = forms.ChoiceField(
+        choices=CHOICES, 
+        widget=forms.RadioSelect,
+        label=_('Вкажіть ваш статус:'),
+        required=True
+        )
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ('username', 'password1', 'password2', 'status')
--- users/views.py
+++ users/views.py
@@ -1,2 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from .forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 
+class RegisterView(View):
+
+    template_name = 'registration/registration.html'
+
+    def get(self, request):
+        ctx = {
+            'form' : UserCreationForm()
+        }
+        return render(request, self.template_name, ctx)
+
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('main')
+
+        ctx = {
+            'form' : form
+        }
+        return render(request, self.template_name, ctx)
