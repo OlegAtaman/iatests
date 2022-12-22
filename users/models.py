004944578ce49c6fd7bc0da1797e836a5a8c10eb
Dmytro <supezbiz@gmail.com>
2022-12-22 07:03:00 +0200

    Add answers overview functionality

--- users/models.py
+++ users/models.py
@@ -1,10 +1,10 @@
-from email.policy import default
-from unittest.util import _MAX_LENGTH
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
 from testapp.models import Course
 
+
 class User(AbstractUser):
-    CHOICES = [('S', 'Студент'), ('T', 'Викладач')]
-    status = models.CharField(choices=CHOICES, max_length=1, default='S')
+    CHOICES = [("S", "Студент"), ("T", "Викладач")]
+    status = models.CharField(choices=CHOICES, max_length=1, default="S")
     pass
